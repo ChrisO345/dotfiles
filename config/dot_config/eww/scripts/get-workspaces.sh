b9ae7e35e84b462709bcb04a3cc695912da07de1
Chris <chrisoliver345@gmail.com>
2025-06-18 18:27:38 +1200

    Changed eww colors, made active workspaces more visible

--- config/dot_config/eww/scripts/get-workspaces.sh
+++ config/dot_config/eww/scripts/get-workspaces.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
-# hyprctl workspaces -j | jq -c '
-#   sort_by(.id) as $workspaces |
-#   [range(1; 6) as $id |
-#     ($workspaces[] | select(.id == $id)) // {id: $id, name: "Workspace \($id)", windows: 0}
-#   ]
-# '
-hyprctl workspaces -j | jq -c 'sort_by(.id)'
+hyprctl workspaces -j | jq -c '
+  sort_by(.id) as $ws |
+  (
+    [range(1;6) as $i |
+      ($ws[] | select(.id == $i)) // {id: $i, name: "Workspace \($i)", windows: 0}
+    ] + [
+      $ws[] | select(.id >= 6)
+    ]
+  )
+'
 
 socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-  hyprctl workspaces -j | jq -c 'sort_by(.id)'
+hyprctl workspaces -j | jq -c '
+  sort_by(.id) as $ws |
+  (
+    [range(1;6) as $i |
+      ($ws[] | select(.id == $i)) // {id: $i, name: "Workspace \($i)", windows: 0}
+    ] + [
+      $ws[] | select(.id >= 6)
+    ]
+  )
+'
 done
--- config/dot_config/shell/alias.sh
+++ config/dot_config/shell/alias.sh
@@ -3,13 +3,17 @@ alias cls="clear"
 alias v="nvim"
 alias shell="exec \$SHELL -l"
 
+# Neovim aliases
+alias vf='nvim $(fd . -t f | fzf)'
+alias vfm='nvim $(fd . -t f | fzf -m)'
+
 # File & Directory Operations
 alias mv="mv -i"
 alias rm="rm -Iv"
 alias df="df -h"
 alias du="du -h -d 1"
 
-# Variants, TODO: get eza installed and working
+# Variants
 alias ls="eza --icons --group-directories-first"
 alias la="eza -a --icons --group-directories-first"
 alias cd="z"
