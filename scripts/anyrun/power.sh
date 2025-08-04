411598bf3583fca7ba658b07f015be46bfec9cc6
Chris <chrisoliver345@gmail.com>
2025-08-04 20:51:32 +1200

    Updates

--- scripts/anyrun/power.sh
+++ scripts/anyrun/power.sh
@@ -4,9 +4,10 @@ OPTIONS=(
   "Shutdown"
   "Restart"
   "Logout"
+  "Lock"
 )
 
-ANYRUN_PLUGINS="/home/chris/.config/anyrun/lib/libstdin.so"
+ANYRUN_PLUGINS="$XDG_CONFIG_HOME/anyrun/lib/libstdin.so"
 
 # Construct the menu options string
 MENU_OPTIONS=""
@@ -16,24 +17,17 @@ done
 MENU_OPTIONS="${MENU_OPTIONS%\\n}"
 
 # Check if anyrun is installed
-if ! command -v /home/chris/.local/share/cargo/bin/anyrun &>/dev/null; then
+if ! command -v $CARGO_HOME/bin/anyrun &>/dev/null; then
   notify-send "anyrun not found" "Please install anyrun to use this script."
   exit 1
 fi
 
-selected_option=$(echo -e "$MENU_OPTIONS" |  /home/chris/.local/share/cargo/bin/anyrun --plugins "$ANYRUN_PLUGINS")
+selected_option=$(echo -e "$MENU_OPTIONS" |  $CARGO_HOME/bin/anyrun --plugins "$ANYRUN_PLUGINS")
 
 case "$selected_option" in
-  "Shutdown")
-    shutdown now
-    ;;
-  "Restart")
-    shutdown -r now
-    ;;
-  "Logout")
-    hyprctl dispatch exit
-    ;;
-  *)
-    # No valid selection or user cancelled
-    ;;
+  "Shutdown") shutdown now;;
+  "Restart") shutdown -r now;;
+  "Logout") hyprctl dispatch exit;;
+  "Lock") hyprlock;;
+  *) ;; # No valid selection or user cancelled
 esac
